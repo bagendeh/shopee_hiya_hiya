9d123a18ff0a5aaceee075b193db5cc571a7c24a
nandawijayaputra <42146158+bagendeh@users.noreply.github.com>
2021-12-12 10:53:27 +0700

    yu
    y

---
+++ config.py
@@ -0,0 +1,6 @@
+
+pin_number ="090603"
+#link_produk ="https://shopee.co.id/-COD-Micropack-Powerbank-10000-mAh-Fast-Charging-for-Oppo-VIVO-Samsung-Xiaomi-Redmi-iPhone-i.98362995.7245279462"
+link_produk ="https://shopee.co.id/Sepeda-Lipat-Vivacycle-Misty-16-Folding-7-Sp-i.36073630.5876014363"
+#cookiee = 'b0dEEsaI6h/BuT7Zfnad+M5H9fLdTNzV+PrCVm5s+UVZQfQoRrnXw7NS5SZXycgkzk3m8aqHRf+fuZEkUGnqmvCH7ak5AQFxHC4PIoIKqpwZ9S1k4HuPbTmZ89XcNnW9Sx5h+HUmilsVcBOCcVkR2ofoC5fzz0lk3mFcwYfW3sY='
+cookiee = 'VXhZM3JadExKSkJyWUpKMevrLw9C8V7uFJSUtO0zUA+rle0cHgRsdb3orI0Gu28+ibLl6j12YFV6machnoz5SJx9x22h0emp3L3xkRzASkyoExGliwZNTz24vDJVWWr/xuMtphZz3vpxm4+vnLzvT+1Il4Zs1cDGZaKjXw1Cn6E='
